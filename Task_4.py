88cbaa4494c864946ce84a5f644e73652e62170a
Lidiia Mohylna <lidiia.mohylna@oracle.com>
2025-06-30 14:14:46 +0300

    commit "home work" 2

--- Task_4.py
+++ Task_4.py
@@ -17,24 +17,21 @@ def get_ucoming_birthdays(users):
         current_year_birthday = date(current_date.year,  user_birthday.month, user_birthday.day)
         difference = (current_year_birthday - current_date).days
         birthday_week_day = current_year_birthday.weekday()
-        if current_year_birthday < current_date:
-           new_congratulation_date1 = current_year_birthday + timedelta(days=365)
-           new_congratulation_date1_formatted = new_congratulation_date1.strftime("%Y.%m.%d")
-           new_user_data1 = {"name": user["name"], "congratulation_date": new_congratulation_date1_formatted}
-           next_birthdays.append(new_user_data1)
-           #не дуже було зрозумило з завдання що з цією перевіркою робити: 
-           # "чи вже минув день народження в цьому році. Якщо так, розгляньте дату на наступний рік."
-           # тому залишила дату наступного року при виведенні
-        elif difference <= 7 and user_birthday.month >= current_date.month:
-            current_year_birthday_formatted = current_year_birthday.strftime("%Y.%m.%d")
-            new_user_data2 = {"name": user["name"], "congratulation_date": current_year_birthday_formatted}
-            next_birthdays.append(new_user_data2)
-            if birthday_week_day == 6:
-                new_congratulation_date2 = current_year_birthday + timedelta(days=1)
-                new_congratulation_date2_formatted = new_congratulation_date2.strftime("%Y.%m.%d")
-                new_user_data3 = {"name": user["name"], "congratulation_date": new_congratulation_date2_formatted}
-                next_birthdays.append(new_user_data3)
-       
+        if current_year_birthday >= current_date and difference < 7 :
+            new_user_data = {"name": user["name"], "congratulation_date": current_year_birthday.strftime("%Y.%m.%d")}
+            next_birthdays.append(new_user_data)
+        elif current_year_birthday >= current_date and difference < 7 and birthday_week_day == 6:
+            new_congratulation_date = (current_year_birthday + timedelta(days=1)).strftime("%Y.%m.%d")
+            new_user_data = {"name": user["name"], "congratulation_date": new_congratulation_date}
+            next_birthdays.append(new_user_data)
+        else:
+            if current_year_birthday < current_date:
+                new_congratulation_date = (current_year_birthday + timedelta(days=365)).strftime("%Y.%m.%d")
+                new_user_data = {"name": user["name"], "congratulation_date": new_congratulation_date}
+                next_birthdays.append(new_user_data)
+             #не дуже було зрозумило з завдання що з цією перевіркою робити: 
+             # "чи вже минув день народження в цьому році. Якщо так, розгляньте дату на наступний рік."
+             # тому залишила дату наступного року при виведенні
            
     return next_birthdays   
 
